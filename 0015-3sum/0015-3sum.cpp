40f6fd56c907e0d05099ed8e0b9b0889ec8d7be7
Ayush Yadav <ayushyadav190206@gmail.com>
2026-07-10 19:48:08 +0530

    Update 0015-3sum.cpp

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
+        //two pointer approach
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());                   
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;       //so that the value do not repeat
             int j=i+1,k=n-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
@@ -17,7 +18,7 @@ public:
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[j]==nums[j-1]) j++;     //so that the value do not repeat
                 }
             }
         }
