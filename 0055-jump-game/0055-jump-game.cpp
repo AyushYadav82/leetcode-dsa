5b0eda03a61e2d8d6b8bd489cfc7e9106ac96e3b
Ayush Yadav <ayushyadav190206@gmail.com>
2026-08-21 20:56:45 +0530

    Time: 0 ms (100%), Space: 52.5 MB (16.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,13 +3,9 @@ public:
     bool canJump(vector<int>& nums) {
         int far=0;
         for(int i=0;i<nums.size();i++){
-            if(i>far){
-                return false;
-            }
+            if(far<i) return false;
             far=max(far,i+nums[i]);
-            if(far>=nums.size()-1){
-                return true;
-            }
+            if(far>nums.size()) return true;
         }
         return true;
     }
