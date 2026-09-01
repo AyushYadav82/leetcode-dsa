0948121606272fb7e60d1ffd7837c1af2779682a
Ayush Yadav <ayushyadav190206@gmail.com>
2026-09-01 19:37:29 +0530

    Time: 3 ms (11.64%), Space: 8 MB (39.36%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOperations(int nums1, int nums2) {
+        int count=0;
+        while(nums1!=0 && nums2!=0){
+            if(nums1>=nums2){
+                nums1=nums1-nums2;
+            }else if(nums2>=nums1){
+                nums2=nums2-nums1;
+            }
+            count++;
+        }
+        return count;
+    }
+};
