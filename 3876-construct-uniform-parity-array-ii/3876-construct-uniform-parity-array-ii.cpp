9c094c2dc3c2cd5ba5e8f96032bce8960c940155
Ayush Yadav <ayushyadav190206@gmail.com>
2026-09-03 16:05:41 +0530

    Time: 65 ms (23.56%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        sort(nums1.begin(),nums1.end());
+        if(nums1[0]%2!=0) return true;
+        for(int i:nums1){
+            if(i%2!=0) return false;
+        }
+        return true;
+    }
+};
