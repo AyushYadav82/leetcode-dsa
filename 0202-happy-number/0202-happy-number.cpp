0747754e57ef70b1abe7087a6b6eb438599eb6ec
Ayush Yadav <ayushyadav190206@gmail.com>
2026-09-15 09:42:41 +0530

    Time: 1 ms (26.29%), Space: 8.3 MB (49.28%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        set<int> st;
+        while(n!=1){
+            if(st.count(n)) return false;
+            st.insert(n);
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                n/=10;
+                sum+=digit*digit;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
